118a422af24e0707364e6a3a06d159a0d35b0202
Casperfeng <casp.feng@gmail.com>
2019-01-16 21:38:03 +0100

    updated UI

--- app.js
+++ app.js
@@ -23,6 +23,9 @@ const ItemCtrl = (function(){
 
   // Public methods
   return {
+    getItems: function(){
+      return data.items;
+    },
     logData: function(){
       return data;
     }
@@ -33,10 +36,27 @@ const ItemCtrl = (function(){
 
 // UI Controller
 const UICtrl = (function(){
+  const UISelectors = {
+    itemList: '#item-list'
+  }
   
   // Public methods
   return {
+    populateItemList: function(items){
+      let html = '';
 
+      items.forEach(function(item){
+        html += `<li class="collection-item" id="item-${item.id}">
+        <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+        <a href="#" class="secondary-content">
+          <i class="edit-item fa fa-pencil"></i>
+        </a>
+      </li>`;
+      });
+
+      // Insert list items
+      document.querySelector(UISelectors.itemList).innerHTML = html;
+    }
   }
 })();
 
@@ -48,7 +68,11 @@ const App = (function(ItemCtrl, UICtrl){
   // Public methods
   return {
     init: function(){
-      console.log('Initializing App...');
+      // Fetch items from data structure
+      const items = ItemCtrl.getItems();
+
+      // Populate list with items
+      UICtrl.populateItemList(items);
     }
   }
   
